8356fa649cc69582578d9d0988a3cecc36a2bba3
Musaed Alsuwaidan <musaed919@gmail.com>
2018-02-19 18:31:09 -0600

    onSignIn() works

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
+var firebase = require("firebase");
 var http = require('http');
-var firebase = require('firebase');
-var fs = require('fs');
+var  fs = require('fs');
 
-firebase.initializeApp({
-	apiKey: 'AIzaSyBlfPXpbD7C_bcpvNQFjYOTqmPEHRZy-ws',
-	authDomain: 'cs-595-cpd.firebaseapp.com',
-	databaseURL: 'https://cs-595-cpd.firebaseio.com',
-	projectId: 'cs-595-cpd',
-	storageBucket: 'cs-595-cpd.appspot.com',
-	messagingSenderId: '192816903778'
-});
 
-http.createServer(function(req, res){
-	console.log("ENTRY");
-	fs.readFile('./index.html', function(err, data){
-		console.log("READING PAGE");
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
-		return res.end();
-	});
-	console.log("EXIT");
+var config = {
+  apiKey: "AIzaSyBlfPXpbD7C_bcpvNQFjYOTqmPEHRZy-ws",
+  authDomain: "cs-595-cpd.firebaseapp.com",
+  databaseURL: "https://cs-595-cpd.firebaseio.com",
+  projectId: "cs-595-cpd",
+  storageBucket: "cs-595-cpd.appspot.com",
+  messagingSenderId: "192816903778"
+};
+firebase.initializeApp(config);
+
+
+http.createServer(function (req, res) {
+  fs.readFile("index.html", function(err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 }).listen(8080);
